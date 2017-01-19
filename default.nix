d54b7d01bf445d9f961272f74213929de4b2f661
Linus Heckemann <git@sphalerite.org>
2017-01-19 11:15:06 +0000

    Add dunst for notifications

--- default.nix
+++ default.nix
@@ -7,6 +7,11 @@ let
     #!${pkgs.stdenv.shell}
     ${redshift}/bin/redshift -l 56:-4 -t 5500:2800 &
     [[ -r $HOME/.background-image ]] && ${pkgs.feh}/bin/feh --bg-scale $HOME/.background-image
+    ${pkgs.dunst}/bin/dunst \
+        -padding 5 \
+        -horizontal_padding 10 \
+        -dmenu ${pkgs.dmenu}/bin/dmenu \
+        -context_key XF86LaunchB &
     exec ${i3}/bin/i3
   '';
 in
