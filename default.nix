5c0a9c8dc0e691df7c9ee1d9cfd523beff154b01
Linus Heckemann <git@sphalerite.org>
2017-01-19 11:14:56 +0000

    redshift: fix coordinates

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ let
   i3 = import ./i3.nix { inherit pkgs; };
   xsession = pkgs.writeScriptBin "xsession" ''
     #!${pkgs.stdenv.shell}
-    ${redshift}/bin/redshift -l 55:4 -t 5500:2800 &
+    ${redshift}/bin/redshift -l 56:-4 -t 5500:2800 &
     [[ -r $HOME/.background-image ]] && ${pkgs.feh}/bin/feh --bg-scale $HOME/.background-image
     exec ${i3}/bin/i3
   '';
