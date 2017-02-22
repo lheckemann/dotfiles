098d815535e069b0611ad9e63416a9fe7c977e9d
Linus Heckemann <git@sphalerite.org>
2017-02-22 13:31:36 +0000

    Filter vim plugins by filetype

--- neovim.nix
+++ neovim.nix
@@ -12,18 +12,27 @@
             "Supertab"
             "The_NERD_tree"
             "deoplete-nvim"
-            "deoplete-jedi"
             "vim-addon-vim2nix"
             "jellybeans"
             "vim-nix"
+            "rust-vim"
           ];
+        }
+        {
+          name = "deoplete-jedi";
+          ft_regex = "python";
+        }
+        {
+          name = "rust-vim";
+          filename_regex = "\(.*\.rs\|Cargo\.\(toml\|lock\)\)$";
         }
       ];
       vam.knownPlugins = pkgs.vimPlugins // {
         jellybeans = pkgs.vimUtils.buildVimPluginFrom2Nix {
           name = "jellybeans-2016-10-18";
-          src = pkgs.fetchgit {
-            url = "git://github.com/nanotech/jellybeans.vim";
+          src = pkgs.fetchFromGitHub {
+            owner = "nanotech";
+            repo = "jellybeans.vim";
             rev = "fd089ca8a242263f61ae7bddce55a007d535bc65";
             sha256 = "00knmhmfw9d04p076cy0k5hglk7ly36biai8p9b6l9762irhzypp";
           };
