d15d0701dbae7f58cef3c8d513622c0878723970
Linus Heckemann <git@sphalerite.org>
2017-09-07 13:49:51 +0100

    Make dunst bigger

--- graphical.nix
+++ graphical.nix
@@ -7,9 +7,10 @@ let
     ${pkgs.redshift}/bin/redshift -l 56:-4 -t 5500:2800 &
     [[ -r $HOME/.background-image ]] && ${pkgs.feh}/bin/feh --bg-max $HOME/.background-image
     ${pkgs.dunst}/bin/dunst \
-        -padding 5 \
-        -horizontal_padding 10 \
+        -padding 15 \
+        -horizontal_padding 20 \
         -dmenu ${pkgs.dmenu}/bin/dmenu \
+        -fn "Liberation Sans 24" \
         -context_key XF86LaunchB &
     exec ${i3Configured}/bin/i3
   '';
