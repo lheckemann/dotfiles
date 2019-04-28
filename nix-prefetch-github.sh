ecda36e014b6ff2882273a8e492d466c960231a5
Linus Heckemann <git@sphalerite.org>
2019-04-28 10:22:14 +0200

    nix-prefetch-github: allow uppercase letters

--- nix-prefetch-github.sh
+++ nix-prefetch-github.sh
@@ -2,7 +2,7 @@
 set -ex
 url="$1"
 branch="${2:-master}"
-[[ "$url" =~ ^https://github.com/[a-z0-9_-]*/[a-z0-9_-]* ]]
+[[ "$url" =~ ^https://github.com/[A-Za-z0-9_-]*/[A-Za-z0-9_-]* ]]
 path="${url#https://github.com/}"
 owner="${path%%/*}"
 repo="${path#*/}"
