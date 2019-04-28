3f60def12b5db6bbfea3d0c93a650ebda5395fe3
Linus Heckemann <git@sphalerite.org>
2019-04-28 10:18:05 +0200

    reorganise: move graphical.nix into default.nix

--- default.nix
+++ default.nix
@@ -33,57 +33,100 @@ let
     export PATH=${lib.escapeShellArg (lib.makeBinPath [git bash nix gnutar gzip coreutils])}
     exec bash ${./nix-prefetch-github.sh} "$@"
   '';
-in
-  {
-    inherit
-      confs
-      tmuxConfigured
-      tmuxMan
-      zshrc
-      nix-prefetch-github
-      openPort
-      ;
-    inherit (pkgs)
-      binutils # mostly for strings
-      fbterm
-      fd
-      fzf
-      gdb
-      syncthing
-      htop
-      inotify-tools
-      indent
-      isync
-      jq
-      khal
-      lsof
-      man-pages
-      mcabber
-      moreutils
-      mosh
-      msmtp
-      ncdu
-      neomutt
-      nethack
-      notmuch
-      nix-index
-      nixops
-      nmap
-      pandoc
-      potrace
-      ripgrep
-      scaleway-cli
-      sqliteInteractive
-      sshfs
-      sshuttle
-      tmuxp
-      units
-      unzip
-      vagrant
-      vdirsyncer
-      youtube-dl
-      ;
-    inherit (bind) dnsutils;
-    gnupg = gnupg.override {guiSupport = false;};
-    texlive = texlive.combined.scheme-small;
-  }
+  src = writeShellScriptBin "src" ''
+    set -x
+    test -n "$1"
+    exec nix-shell '<nixpkgs>' -A "$1" --run 'runHook unpackPhase'
+  '';
+
+in rec {
+basicLight = {
+  inherit confs tmuxConfigured nix-prefetch-github src openPort;
+  inherit (pkgs) binutils fd htop jq lsof moreutils ncdu strace units;
+  inherit (pkgs.bind) dnsutils;
+};
+basic = basicLight // {
+  # So that `nix-env -f dotfiles -i` installs the basic set
+  recurseForDerivations = true;
+  inherit (pkgs) ripgrep;
+};
+desktop-nographic = basic // {
+  inherit (pkgs)
+    gdb
+    syncthing
+    inotify-tools
+    isync
+    khal
+    man-pages
+    mcabber
+    mosh
+    msmtp
+    notmuch
+    nix-index
+    nixops
+    nmap
+    pandoc
+    potrace
+    ripgrep
+    sqliteInteractive
+    sshfs
+    sshuttle
+    tmuxp
+    unzip
+    vagrant
+    vdirsyncer
+    ;
+  gnupg = gnupg.override {guiSupport = false;};
+  texlive = texlive.combined.scheme-small;
+};
+desktop-full = desktop-nographic // {
+  inherit (pkgs)
+    alacritty audacity chromium compton dfeet dmenu endless-sky
+    evince feh firefox gimp graphicsmagick
+    gnupg # Replace the non-graphical one from desktop-nographic
+    i3status inkscape kvm libreoffice mpv mumble mupdf noto-fonts
+    noto-fonts-emoji pass pavucontrol redshift scrot socat
+    tdesktop terminus_font vlc xidlehook xsel youtube-dl
+    ;
+  inherit (gnome3) eog dconf;
+  inherit (python3Packages) binwalk;
+  emacs = callPackage ./emacs.nix {};
+  lock = callPackage ./locker {};
+  i3 = lib.lowPrio i3;
+  i3Configured = lib.hiPrio (
+    runCommand "i3-with-config" {
+      nativeBuildInputs = [ makeWrapper ];
+    } ''
+      mkdir -p $out/bin
+      mkdir -p $out/etc
+      cp ${./i3/config} $out/etc/i3.conf
+      cp ${./i3/status.conf} $out/etc/i3status.conf
+      makeWrapper ${i3}/bin/i3 $out/bin/i3 --add-flags "-c ~/.nix-profile/etc/i3.conf"
+      makeWrapper ${i3status}/bin/i3status $out/bin/i3status --add-flags '-c ~/.nix-profile/etc/i3status.conf'
+      install -Dm0755 ${./status.sh} $out/bin/wrap-i3status
+    ''
+  );
+  xsession = writeScriptBin "xsession" ''
+    #!${stdenv.shell}
+    export XCURSOR_PATH=${gnome3.adwaita-icon-theme}/share/icons \
+           SSH_AUTH_SOCK=/run/user/1000/gnupg/S.gpg-agent.ssh \
+           EDITOR='emacsclient -a ""'
+    xrdb -merge - <<EOF
+    Xcursor.theme: Adwaita
+    EOF
+    [[ -r $HOME/.background-image ]] && ${feh}/bin/feh --bg-max $HOME/.background-image
+    ${dunst}/bin/dunst \
+        -padding 15 \
+        -horizontal_padding 20 \
+        -dmenu ${dmenu}/bin/dmenu \
+        -fn "Liberation Sans 18" \
+        -history_key Redo \
+        -context_key SunProps &
+    gpg-connect-agent /bye
+    exec ${i3Configured}/bin/i3
+  '';
+  switch-user = writeScriptBin "switch-user" ''
+    ${dbus}/bin/dbus-send --print-reply --system --dest=org.freedesktop.DisplayManager /org/freedesktop/DisplayManager/Seat0 org.freedesktop.DisplayManager.Seat.SwitchToGreeter
+  '';
+};
+}
