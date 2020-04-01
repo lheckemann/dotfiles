795bc3fc6c9bd9688022452541996b9e339b4c58
Linus Heckemann <git@sphalerite.org>
2020-04-01 10:00:21 +0200

    emacs-wayland: use upstream git repo

--- emacs-wayland.nix
+++ emacs-wayland.nix
@@ -3,19 +3,19 @@
 enableDebugging (emacs26.overrideAttrs (
   { buildInputs, configureFlags ? [], postPatch ? "", nativeBuildInputs ? [], ... }:
   {
-    src = builtins.fetchGit ~/projects/emacs;
     /*
+    src = builtins.fetchGit ~/projects/emacs;
     src = builtins.fetchGit {
       url = "https://github.com/masm11/emacs";
       ref = "pgtk";
     };
+    */
     src = fetchFromGitHub {
       owner = "masm11";
       repo = "emacs";
-      rev = "4fd0e8e06ea8fc12e8c995810fd407719465fa6a";
-      sha256 = "1zsmwd0qksvls677sgm7i609vij7fcr60lq77xkszqx0fx3f6g88";
+      rev = "f88789bd3e87111809e3db25e2604a0f58c17e92";
+      sha256 = "10m40qpvnbxy98h84lvlp1f5zpqqarbxihjn7x1v4072hf2fhj3q";
     };
-    */
 
     patches = [];
     buildInputs = buildInputs ++ [ wayland wayland-protocols];
