5e804eabcb17bd61c61d755a93b410920580ab39
Linus Heckemann <git@sphalerite.org>
2020-07-16 16:56:31 +0200

    nixops-wrapper: confirm with single keypress

--- nixops-wrapper.nix
+++ nixops-wrapper.nix
@@ -1,13 +1,15 @@
-{ writeShellScriptBin, nixops
+{ writeScriptBin, nixops, bashInteractive
 , name
 , requireConfirm ? true
 , pre ? ""
 }:
-writeShellScriptBin name ''
+writeScriptBin name ''
+  #!${bashInteractive}${bashInteractive.shellPath}
   export NIXOPS_DEPLOYMENT=${name}
   ${pre}
   if [[ " $*" = *\ deploy\ * ]] && [[ "$*" != *\ --dry-activate* ]] && [[ "$*" != *\ --build-only* ]] ; then
-     read -p "really deploy? " confirm
+     read -p "really deploy? " -N1 confirm
+     echo
      [[ $confirm = y ]] || exit
   fi
   exec nixops "$@"
