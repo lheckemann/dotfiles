38ee0cc7fb71a1781f1e3e07f110a6dd8d5dd570
Linus Heckemann <git@sphalerite.org>
2020-11-22 16:23:56 +0100

    fancier nixops warpper
    with shellcheck!

--- nixops-wrapper.nix
+++ nixops-wrapper.nix
@@ -1,16 +1,29 @@
-{ writeScriptBin, nixops, bashInteractive
+{ writeTextFile, nixops, bashInteractive, shellcheck
 , name
 , requireConfirm ? true
 , pre ? ""
 }:
-writeScriptBin name ''
-  #!${bashInteractive}${bashInteractive.shellPath}
-  export NIXOPS_DEPLOYMENT=${name}
-  ${pre}
-  if [[ " $*" = *\ deploy\ * ]] && [[ "$*" != *\ --dry-activate* ]] && [[ "$*" != *\ --build-only* ]] ; then
-     read -p "really deploy? " -N1 confirm
-     echo
-     [[ $confirm = y ]] || exit
-  fi
-  exec nixops "$@"
-''
+writeTextFile {
+  inherit name;
+  destination = "/bin/${name}";
+  executable = true;
+  checkPhase = ''
+    ${shellcheck}/bin/shellcheck $out/bin/${name}
+  '';
+  text = ''
+    #!${bashInteractive}${bashInteractive.shellPath}
+    export NIXOPS_DEPLOYMENT=${name}
+    ${pre}
+    if [[ " $*" = *\ deploy\ * ]]; then
+      case "$*" in
+        *\ --dry-activate* | *\ --build-only* | *\ --dry-run*) ;;
+        *)
+          read -rp "really deploy? " -N1 confirm
+          echo
+          [[ $confirm = y ]] || exit
+          ;;
+      esac
+    fi
+    exec nixops "$@"
+  '';
+}
