b3df337de4e92cfd5b1e7ae3b3e8c60dc3c3ee37
Linus Heckemann <git@sphalerite.org>
2020-11-22 16:22:15 +0100

    add rsync to basicLight

--- default.nix
+++ default.nix
@@ -74,7 +74,7 @@ let
 in rec {
 basicLight = {
   inherit confs tmuxConfigured nix-prefetch-github src openPort vim;
-  inherit (pkgs) binutils file htop jq lsof moreutils ncdu pv strace tcpdump units;
+  inherit (pkgs) binutils file htop jq lsof moreutils ncdu pv rsync strace tcpdump units;
   inherit (pkgs) lnav;
   inherit (pkgs.bind) dnsutils;
   build-on = pkgs.writeShellScriptBin "build-on" ''
