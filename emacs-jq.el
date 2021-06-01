97e2f4ff979f2bc7ba5bb7a025c100cef20d25c1
Linus Heckemann <git@sphalerite.org>
2021-06-02 00:04:23 +0200

    emacs-jq: output strings directly

--- emacs-jq.el
+++ emacs-jq.el
@@ -1,5 +1,5 @@
 (defun jq-on-change (begin end prev-len) "" nil
   (write-region (point-min) (point-max) "/scratch/query.jq" nil nil nil nil)
-  (shell-command "jq -f /scratch/query.jq </scratch/activity.json" "*jq output*" "*jq output*"))
+  (shell-command "jq -rf /scratch/query.jq </scratch/activity.json" "*jq output*" "*jq output*"))
 (defun enable-jq-on-change () ""
   (add-hook 'after-change-functions 'jq-on-change nil t))
