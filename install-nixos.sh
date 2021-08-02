511816df5fa07e438415c0efe429ef29a0b0ac6f
Linus Heckemann <git@sphalerite.org>
2021-08-02 16:53:41 +0200

    install-nixos: add option for mounting target filesystems

--- install-nixos.sh
+++ install-nixos.sh
@@ -4,7 +4,7 @@ set -exuo pipefail
 PROGRAM_NAME="$0"
 
 inst() {
-    local system="" host="" action="install" from="auto"
+    local system="" host="" action="install" from="auto" mount=""
     local -a nixCopyArgs
     while [[ "$#" -gt 0 ]] ; do
         case "$1" in
@@ -28,9 +28,13 @@ inst() {
                 from="$2"
                 shift 2
                 ;;
+            -m)
+                mount=1
+                shift
+                ;;
             *)
                 echo 'Usage: $PROGRAM_NAME -s <system> -h <host> [-a <action>] [--substitute] [-f <store>]'
-                exit -1
+                exit 1
                 ;;
         esac
     done
@@ -38,9 +42,20 @@ inst() {
     if [[ "$system" = *.drv ]]; then
         system=$(nix-store --store "$from" -r "$system")
     fi
+
+    if [[ -n "$mount" && "$action" != "install" ]] ; then
+        echo "-m specified, but action isn't install"
+        exit 1
+    fi
+
     nix copy "${nixCopyArgs[@]}" --from "$from" --to ssh://root@"$host" "$system"
     case "$action" in
         install)
+            if [[ -n "$mount" ]] ; then
+                ssh root@"$host" findmnt /mnt && ssh root@"$host" umount -Rlv /mnt
+                ssh root@"$host" mount -v -T "$system/etc/fstab" --target-prefix /mnt -o X-mount.mkdir /
+                ssh root@"$host" mount -v -T "$system/etc/fstab" --target-prefix /mnt -o X-mount.mkdir --all
+            fi
             ssh -t root@"$host" nix copy --no-require-sigs "$system" --to /mnt
             ssh root@"$host" nixos-install --no-root-passwd --no-channel-copy --system "$system"
             ;;
