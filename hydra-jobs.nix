2d4879c537494cdbaca9eaba6685ad19af3b4045
Linus Heckemann <git@sphalerite.org>
2021-09-24 17:43:17 +0200

    WIP hydra

---
+++ hydra-jobs.nix
@@ -0,0 +1,47 @@
+{ nixpkgs ? <nixpkgs>
+, pkgs ? import nixpkgs {}
+, declInput ? {}
+}:
+rec {
+  defaults = {
+    enabled = "1";
+    hidden = false;
+    nixexprinput = "nix-config";
+    keepnr = 50;
+    schedulingshares = 100;
+    checkinterval = 1800;
+    type = 0;
+    inputs = {
+      nix-config = {
+        type = "git";
+        value = "https://git.sr.ht/~linuxhackerman/nix-config master";
+        emailresponsible = false;
+      };
+    };
+  };
+  jobsets = rec {
+    sources = lib.recursiveUpdate defaults {
+      nixexprpath = "nix/sources.nix";
+    };
+    /*
+    dotfiles = {
+      nixexprpath = "default.nix";
+      keepnr = 50;
+      schedulingshares = 100;
+      checkinterval = 1800;
+      type = 0;
+      inputs = {
+        nix-config = {
+          type = "git";
+          value = "https://git.sr.ht/~linuxhackerman/nix-config master";
+          emailresponsible = false;
+        };
+        nixpkgs = {
+          type = "git";
+          value = "https://github.com/nixos/nixpkgs nixos-21.05";
+        };
+      };
+    };*/
+  };
+  jobsetsJSON = (pkgs.formats.json {}).generate jobsets;
+}
