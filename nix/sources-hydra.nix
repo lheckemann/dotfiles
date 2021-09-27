d68814b69745cc383b05d277c93ba7c555a5db76
Linus Heckemann <git@sphalerite.org>
2021-09-27 15:15:15 +0200

    fix syntax, again

--- nix/sources-hydra.nix
+++ nix/sources-hydra.nix
@@ -1,3 +1,3 @@
 { nixpkgs }:
 let lib = import (nixpkgs + "/lib");
-in lib.mapAttrs (_: value: value.outPath) (import ./sources.nix { pkgs = import nixpkgs {}; });
+in lib.mapAttrs (_: value: value.outPath) (import ./sources.nix { pkgs = import nixpkgs {}; })
