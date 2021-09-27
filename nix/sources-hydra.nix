c78e780d8ff476eb11aed63aad0545d68b901b94
Linus Heckemann <git@sphalerite.org>
2021-09-27 14:53:51 +0200

    Remove unnecessary "sources" layer

--- nix/sources-hydra.nix
+++ nix/sources-hydra.nix
@@ -1,5 +1,3 @@
 { nixpkgs }:
 let lib = import (nixpkgs + "/lib");
-in {
-  sources = lib.mapAttrs (_: value: value.outPath) (import ./sources.nix { pkgs = import nixpkgs {}; });
-}
+in lib.mapAttrs (_: value: value.outPath) (import ./sources.nix { pkgs = import nixpkgs {}; });
